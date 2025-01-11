b8c9958a84eedbbf12112eef7541183cb3aba98c
chr1swill <alexanderwilliams20003@gmail.com>
2025-01-11 14:08:41 -0500

    feat: first commit

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+KOKORO_DIR=Kokoro-82M
+WHISPER_DIR=whisper
+
+if [ ! -d "$KOKORO_DIR" ]; then
+    git clone https://huggingface.co/hexgrad/Kokoro-82M
+fi
+
+if [ ! -d "$WHISPER_DIR" ]; then
+    git clone https://github.com/openai/whisper.git
+fi
+
+python3 -m venv venv
+
+source venv/bin/activate
+
+pip install phonemizer torch transformers scipy munch openai-whisper
+
+echo "You need to install ffmpeg, git-lfs, and espeak-ng"
