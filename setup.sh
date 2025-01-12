e7c7c5de44205e04cfd828709e40947562a0b1df
chr1swill <alexanderwilliams20003@gmail.com>
2025-01-12 17:05:32 -0500

    feat: add more useful information to setup.sh

--- setup.sh
+++ setup.sh
@@ -15,6 +15,10 @@ fi
 
 #source venv/bin/activate
 
-#pip install phonemizer torch transformers scipy munch openai-whisper
 
 echo "You need to install ffmpeg, git-lfs, python3, python3-venv, and espeak-ng"
+echo "Then to: "
+echo "python3 -m venv venv"
+echo "source venv/bin/activate"
+echo "pip install phonemizer torch transformers scipy munch openai-whisper"
+echo "Then you ready to run -> python3 run.py"
